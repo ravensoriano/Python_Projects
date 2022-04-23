2d33f0e1feac6c2d27c2c2d4b496869b1db5b97b
Raven Soriano <102122934+ravensoriano@users.noreply.github.com>
2022-04-23 00:07:33 -0400

    Add Band Name Generator Project

---
+++ band_name_generator/main.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator.")
+
+name_of_city = input ("What's name of the city you grew up in? ")
+print(name_of_city)
+
+favorite_color = input ("What's your favorite color? ")
+print(favorite_color)
+
+band_name = name_of_city + " " + favorite_color
+print("Your band name could be " + band_name)
